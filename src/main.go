833108067eff93f109340a4a354de47e95139227
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2025-07-21 19:51:57 +0200

    feat: add sendReminder function

--- src/main.go
+++ src/main.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
+type Dev struct {
+	name   string
+	userId string
+}
+
 var token string
+
+var devs = []Dev{
+	{
+		name:   "UserName",
+		userId: "uuid",
+	},
+}
+
+func sendReminder(session *discordgo.Session) {
+	for _, dev := range devs {
+		channel, err := session.UserChannelCreate(dev.userId)
+		if err != nil {
+			log.Println("Error creating DM channel:", err)
+			continue
+		}
+
+		message := fmt.Sprintf("Hey %v ! Let's eat a pizza tonight !", dev.name)
+
+		_, err = session.ChannelMessageSend(channel.ID, message)
+		if err != nil {
+			log.Println("Error sending DM:", err)
+		}
+	}
+}
 
 func main() {
 	log.Println("Feed your bot starting")
@@ -21,14 +51,17 @@ func main() {
 	log.Println("Loading successful")
 
 	token = os.Getenv("BOT_TOKEN")
-	discord, _ := discordgo.New("Bot " + token)
-	test := struct {
-		test  *discordgo.Session
-		title string
-	}{
-		discord,
-		"test",
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Error creating discord session", err)
 	}
-	fmt.Println(token[:10])
-	fmt.Println(test.title)
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal("Error opening connection", err)
+	}
+	defer discord.Close()
+
+	log.Println("Feed your dev is running")
+	sendReminder(discord)
 }
